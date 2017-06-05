3900a55896ccbbd702e3a2587866b50b0e56e8ae
Mark Greenwood <mrkjgreenwood@gmail.com>
2017-06-04 22:10:04 -0700

    Minor change to tests

--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@ const assert = require('assert');
 
 describe ('task-service', () => {
   it ('runs tests', () => {
+    // just a 'must pass' test to verify Travis is working
     assert(true);
   });
 });
