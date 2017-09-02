0a73720aab0ede6490a44c21412067ffee2d4073
Mark Greenwood <mrkjgreenwood@gmail.com>
2017-09-02 10:36:45 -0700

    Refactor handlers

--- app/lib/handlers.js
+++ app/lib/handlers.js
@@ -1,27 +1,33 @@
 const R = require('ramda');
 
-
 module.exports = (esClient) => {
+  const listHandler = request => // eslint-disable-line no-unused-vars
+    esClient.search({ index: 'tasks-out', type: 'task', size: 100 })
+      .then(result => result.hits.hits)
+      .then(R.map(R.prop('_source')));
+
+  const getHandler = request =>
+    esClient.get({ index: 'tasks-out', type: 'task', id: request.params.id })
+      .then(R.prop('_source'));
+
+  const postHandler = request =>
+    esClient.index({ index: 'tasks-in', type: 'task', body: request.payload })
+      .then(response => esClient.get({ index: 'tasks-out', type: 'task', id: response._id }))
+      .then(R.prop('_source'));
+
+  const putHandler = request =>
+    esClient.update({ index: 'tasks-in', type: 'task', id: request.params.id, body: { doc: request.payload } })
+      .then(response => esClient.get({ index: 'tasks-out', type: 'task', id: response._id }))
+      .then(R.prop('_source'));
+
+  const deleteHandler = request =>
+    esClient.delete({ index: 'tasks-in', type: 'task', id: request.params.id });
+
   return {
-    listHandler: (request) => // eslint-disable-line no-unused-vars
-      esClient.search({ index: 'tasks-out', type: 'task', size: 100 })
-        .then(result => result.hits.hits)
-        .then(R.map(R.prop('_source'))),
-    getHandler: (request) => // eslint-disable-line no-unused-vars
-      esClient.get({ index: 'tasks-out', type: 'task', id: request.params.id })
-        .then(R.prop('_source')),
-    postHandler: (request) => { // eslint-disable-line no-unused-vars
-      return esClient.index({ index: 'tasks-in', type: 'task', body: request.payload })
-        .then(response => esClient.get({ index: 'tasks-out', type: 'task', id: response._id }))
-        .then(R.prop('_source'));
-    },
-    putHandler: (request) => {
-      return esClient.update({ index: 'tasks-in', type: 'task', id: request.params.id, body: { doc: request.payload } })
-        .then(response => esClient.get({ index: 'tasks-out', type: 'task', id: response._id }))
-        .then(R.prop('_source'));
-    },
-    deleteHandler: (request) => {
-      return esClient.delete({ index: 'tasks-in', type: 'task', id: request.params.id });
-    }
+    listHandler,
+    getHandler,
+    postHandler,
+    putHandler,
+    deleteHandler
   };
 };
